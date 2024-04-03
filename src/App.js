bef539698ba8b46945b8a6272068d6d5706711d0
othonaf <othon.ab@hotmail.com>
2024-04-03 10:42:00 -0300

    Adicionando código de ler cod de barras

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Content from './components/Content';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header" />
+        <Routes>
+          <Route path="/" element={<Content/>} />
+
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
